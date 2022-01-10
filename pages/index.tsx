407a1c4a7b324f955d21fc76f95b0faab9087682
joaovictor <jc7155004@gmail.com>
2022-01-10 13:33:11 -0300

    leo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,103 +1,58 @@
 // import Head from 'next/head'
 
-import { Heading, Grid, Flex, Link, Button, Text } from '@chakra-ui/core'
-import Divider from '../components/Divider'
-import Input from '../components/Input'
+import { Heading, Grid, Flex, Link, Button, Text, Icon, InputGroup, InputRightAddon, Input } from '@chakra-ui/core';
+import Divider from '../components/Divider';
+import {HiOutlineMail} from 'react-icons/hi';
 
 export default function Home() {
   return (
-    <Grid
-      as="main"
-      height="100vh"
-      templateColumns="1fr 480px 480px 1fr"
-      templateRows="1fr 480px 1fr"
-      templateAreas="
-        '. . . .'
-        '. logo form .'
-        '. . . .'
-      "
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Flex gridArea="logo" flexDir="column" alignItems="flex-start">
-        <img src="/rocketseat.svg" alt="Rocketseat" />
+    <Flex
+    h="182px"
+    backgroundColor="gray.300"
+    pt="21px"
+    pb="27"
+    alignItems="center"
+    direction="column"
+  >
+     <Icon as={HiOutlineMail} size="23px" />
 
-        <Heading size="2xl" lineHeight="shorter" marginTop={16}>
-          Faça seu login na plataforma
-        </Heading>
-      </Flex>
+     <Text
+     fontSize='16px'
+      textAlign="center"
+      my="23px"
+      
+     >
+       Cadastre-se e receba as melhores<br/>
+       ofertas no seu email
+     </Text>
+     
 
-      <Flex 
-        gridArea="form"
-        height="100%"
-        backgroundColor="gray.700"
-        borderRadius="md"
-        flexDir="column"
-        alignItems="stretch"
-        padding={16}
-      >
-        <Input
-          placeholder="E-mail"
-        />
+     <InputGroup
+     w="306px"
+ 
+     >
 
-        <Input
-          placeholder="Senha"
-          marginTop={2}
-        />
+       <Input
+       
+       type="text"
+       placeholder="E-mail"
+       _placeholder={{color: 'gray.100'}}
+       borderRadius="21px"
+       backgroundColor="gray.800"
+       />
 
-        <Link
-          alignSelf="flex-start"
-          marginTop={2}
-          fontSize="sm"
-          color="purple.600"
-          fontWeight="bold"
-          _hover={{ color: 'purple.500' }}
-        >
-          Esqueci minha senha
-        </Link>
+         <InputRightAddon
+         color="gray.300"
+         fontWeight="bold"
+         children='Enviar'
+         borderRadius="21px"
+         backgroundColor="gray.600"
+         />
 
-        <Button
-          backgroundColor="purple.500"
-          height="50px"
-          borderRadius="sm"
-          marginTop={6}
-          _hover={{ backgroundColor: 'purple.600' }}
-        >
-          ENTRAR
-        </Button>
+     </InputGroup>
+   
+    </Flex>
 
-        <Text
-          textAlign="center"
-          fontSize="sm"
-          color="gray.300"
-          marginTop={6}
-        >
-          Não tem uma conta? {" "}
-          <Link
-            color="purple.600"
-            fontWeight="bold"
-            _hover={{ color: 'purple.500' }}
-          >
-            Registre-se
-          </Link>
-        </Text>
-
-        <Divider />
-
-        <Flex alignItems="center">
-          <Text fontSize="sm">Ou entre com</Text>
-          <Button
-            height="50px"
-            flex="1"
-            backgroundColor="gray.600"
-            marginLeft={6}
-            borderRadius="sm"
-            _hover={{ backgroundColor: 'purple.500' }}
-          >
-            GITHUB
-          </Button>
-        </Flex>
-      </Flex>
-    </Grid>
+    
   )
 }
